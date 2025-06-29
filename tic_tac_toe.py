5d3f397b8a77815205e5fb74cf5927e3dfbbc2d4
DSnediker <dsnediker7@gmail.com>
2025-06-29 09:12:33 -0700

    created classes, added a way to update the board when an input is given initially

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,10 +1,29 @@
-def print_board():
-    board = [['X',' ','O'],[' ','X','O'],[' ','O','X']]
-    for i in range(len(board)):
-        if i < len(board) - 1:
-            print(' ', board[i][0], ' | ', board[i][1], ' | ', board[i][2], ' ')
-            print('-----------------')
-        else:
-            print(' ', board[i][0], ' | ', board[i][1], ' | ', board[i][2], ' ')
+class Board:
+    def print_board(self,board):
+        for i in range(len(board)):
+            if i < len(board) - 1:
+                print(" ", board[i][0], " | ", board[i][1], " | ", board[i][2], " ")
+                print("-----------------")
+            else:
+                print(" ", board[i][0], " | ", board[i][1], " | ", board[i][2], " ")
 
-print_board()
+
+class TicTacToe:
+    def __init__(self):
+        self.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+
+    def player_action(self):
+
+        player_choice = input("Enter a number (1-9) to mark a square: ")
+
+        pos = int(player_choice) - 1
+        row = pos // 3
+        col = pos % 3
+
+        self.board[row][col] = 'X'
+
+game = TicTacToe()
+board = Board()
+
+game.player_action()
+board.print_board(game.board)
